e9539a327e95407b69b77da3dd27ca7a1cdbe5ae
Your Name <youremail@domain.com>
2025-02-07 14:00:18 +0300

    updated

--- server/app.py
+++ server/app.py
@@ -1,5 +1,30 @@
+# from flask import Flask
+# from flask_migrate import Migrate
+
+# from models import db
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# migrate = Migrate(app, db)
+
+# db.init_app(app)
+
+
+# @app.route('/')
+# def index():
+#     return '<h1>Flask SQLAlchemy Lab 2</h1>'
+
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
+
+
+
 from flask import Flask
 from flask_migrate import Migrate
+from sqlalchemy import inspect
 
 from models import db
 
@@ -15,6 +40,14 @@ db.init_app(app)
 @app.route('/')
 def index():
     return '<h1>Flask SQLAlchemy Lab 2</h1>'
+
+
+@app.cli.command('list-tables')
+def list_tables():
+    """Custom CLI command to list tables in the database."""
+    with app.app_context():
+        inspector = inspect(db.engine)
+        print("Tables in the database:", inspector.get_table_names())
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,3 +1,73 @@
+
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import MetaData
+# from sqlalchemy.ext.associationproxy import association_proxy
+# from sqlalchemy_serializer import SerializerMixin
+
+
+# metadata = MetaData(naming_convention={
+#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+# })
+
+# db = SQLAlchemy(metadata=metadata)
+
+
+# class Customer(db.Model, SerializerMixin):
+#     __tablename__ = 'customers'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+
+#     # Relationship to Review (One to Many)
+#     reviews = db.relationship('Review', back_populates='customer', lazy=True)
+
+#     # Association Proxy for Items
+#     items = association_proxy('reviews', 'item')
+
+#     def __repr__(self):
+#         return f'<Customer {self.id}, {self.name}>'
+
+
+# class Item(db.Model, SerializerMixin):
+#     __tablename__ = 'items'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     price = db.Column(db.Float)
+
+#     # Relationship to Review (One to Many)
+#     reviews = db.relationship('Review', back_populates='item', lazy=True)
+
+#     def __repr__(self):
+#         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+
+# class Review(db.Model):
+#     __tablename__ = 'reviews'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     comment = db.Column(db.String)
+
+#     # Foreign Keys
+#     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+#     item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+#     # Relationships to Customer and Item
+#     customer = db.relationship('Customer', back_populates='reviews')
+#     item = db.relationship('Item', back_populates='reviews')
+
+#     def __repr__(self):
+#         return f'<Review {self.id}, {self.comment}, {self.customer_id}, {self.item_id}>'
+
+#     # SerializerMixin Exclusions to avoid recursion
+#     def to_dict(self):
+#         data = super().to_dict()
+#         data.pop('customer', None)  # Exclude customer from serialized review
+#         data.pop('item', None)  # Exclude item from serialized review
+#         return data
+
+
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
@@ -11,22 +81,52 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    # Relationship to Review
+    reviews = db.relationship('Review', back_populates='customer')
+    # Association proxy for items through reviews
+    items = association_proxy('reviews', 'item')
+
+    serialize_rules = ('-reviews.customer',)
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    # Relationship to Review
+    reviews = db.relationship('Review', back_populates='item')
+
+    serialize_rules = ('-reviews.item',)
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    # Relationships to Customer and Item
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    serialize_rules = ('-customer.reviews', '-item.reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}, Customer {self.customer_id}, Item {self.item_id}>'
